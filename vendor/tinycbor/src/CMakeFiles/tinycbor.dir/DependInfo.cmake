5f0097dd464bddbced4ad7c151c35ea0f76e72e3
Stevens <v585584@TMA0004ALKVTAKZ.fios-router.home>
2017-10-31 12:04:44 -0400

    added tinycbor source

---
+++ vendor/tinycbor/src/CMakeFiles/tinycbor.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cborencoder.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cborencoder.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cborencoder_close_container_checked.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cborencoder_close_container_checked.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cborerrorstrings.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cborerrorstrings.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cborparser.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cborparser.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cborparser_dup_string.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cborparser_dup_string.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cborpretty.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cborpretty.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/cbortojson.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cbortojson.c.o"
+  "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/open_memstream.c" "/Users/v585584/Source/embedded/test_cbor/vendor/tinycbor/src/CMakeFiles/tinycbor.dir/open_memstream.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "vendor/tinycbor/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vendor/tinycbor/src/CMakeFiles/tinycbor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinycbor.dir/cborencoder.c.o"
+  "CMakeFiles/tinycbor.dir/cborerrorstrings.c.o"
+  "CMakeFiles/tinycbor.dir/cborparser_dup_string.c.o"
+  "CMakeFiles/tinycbor.dir/cbortojson.c.o"
+  "CMakeFiles/tinycbor.dir/cborencoder_close_container_checked.c.o"
+  "CMakeFiles/tinycbor.dir/cborparser.c.o"
+  "CMakeFiles/tinycbor.dir/cborpretty.c.o"
+  "CMakeFiles/tinycbor.dir/open_memstream.c.o"
+  "libtinycbor.pdb"
+  "libtinycbor.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinycbor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
